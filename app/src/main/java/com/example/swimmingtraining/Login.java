4f8195ff68ff3436972a640376543dbc80000a90
MussaDev <mussa_sev@mail.ru>
2019-05-26 23:55:26 +0300

    Авторизация с разделением прав с окна логин

--- app/src/main/java/com/example/swimmingtraining/Login.java
+++ app/src/main/java/com/example/swimmingtraining/Login.java
@@ -6,6 +6,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,12 +18,19 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 
 public class Login extends AppCompatActivity {
     EditText email,password;
     Button registerButton,loginButton;
     FirebaseAuth firebaseAuth;
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference user = database.getReference("users");
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -65,8 +73,31 @@ public class Login extends AppCompatActivity {
                             @Override
                             public void onComplete(@NonNull Task<AuthResult> task) {
                                 if(task.isSuccessful()){
-                                    startActivity(new Intent(getApplicationContext(),Main_trainer.class));
-                                    finish();
+                                    //Вход к нужному пользователю
+                                    DatabaseReference uid = user.child(FirebaseAuth.getInstance().getCurrentUser().getUid());
+                                    uid.addValueEventListener(new ValueEventListener() {
+                                        @Override
+                                        public void onDataChange(DataSnapshot dataSnapshot) {
+                                            final String rol1 = dataSnapshot.child("rol").getValue(String.class);
+
+                                            if (rol1.equals("Sportsman")){
+                                                Intent intent = new Intent(Login.this, Main_sportsman.class);
+                                                startActivity(intent);
+                                                finish();
+                                            }
+                                            else if (rol1.equals("Trainer")){
+                                                Intent intent = new Intent(Login.this, Main_trainer.class);
+                                                startActivity(intent);
+                                                finish();
+                                            }
+                                        }
+
+                                        @Override
+                                        public void onCancelled(DatabaseError error) {
+                                            // Failed to read value
+                                            Log.w("Failed to read value.", error.toException());
+                                        }
+                                    });
                                 }
                                 else{
                                     email.getBackground().mutate().setColorFilter(getResources().getColor(R.color.red), PorterDuff.Mode.SRC_ATOP);
